ce22773e4b627a7b597fb9975c49ae1f25eb2349
Shenrick Guzman <165637026+ShenrickGuzman@users.noreply.github.com>
2025-09-26 10:42:42 +0800

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import RulesPopup from '../components/RulesPopup';
 import { Link, useNavigate } from 'react-router-dom';
 import api, { getAssetUrl } from '../lib/api';
